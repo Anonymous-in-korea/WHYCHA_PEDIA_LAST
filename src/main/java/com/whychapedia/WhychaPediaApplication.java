d90545a81939416375426541a51329bbb2fc8b38
Developer <Developer@DESKTOP-MAR4R2H>
2023-03-23 23:08:36 +0900

    23년 3월 23일 와이챠피디아 새 리퍼지토리 생성

---
+++ src/main/java/com/whychapedia/WhychaPediaApplication.java
@@ -0,0 +1,13 @@
+package com.whychapedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhychaPediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhychaPediaApplication.class, args);
+	}
+
+}
