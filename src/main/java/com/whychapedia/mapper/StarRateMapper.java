76b8e66597d5685c93f61cb9f7a9225f73494a0a
easyliving <easyliving@DESKTOP-PLFD5LF>
2023-04-05 17:12:09 +0900

    0405 $이아니고 #

--- src/main/java/com/whychapedia/mapper/StarRateMapper.java
+++ src/main/java/com/whychapedia/mapper/StarRateMapper.java
@@ -11,13 +11,17 @@ public interface StarRateMapper {
 
 	//별점 높은 영화 10개 뽑아오기 	
 	public List<StarRateVo> selectMovieTop(int limit);
-	//영화 별점 넣기
-	public int insertStarRate(int user_id, int movie_id, double star_rate);
+	//영화 별점 넣기(처음)
+	public int insertStarRate(int id,int user_id, int movie_id, double star_rate);
 	//영화 별점 삭제
 	public int deleteStarRate(int user_id, int movie_id);
 	//한명 별점 들고오기
 	public int selectMyStarRate(int user_id, int movie_id);
 	//평가했는지 안했는지 확인하기
 	public int selectIsRating(int user_id, int movie_id);
+	//마지막 별점 고유번호 가져오기
+	public int selectLastId();
+	//별점 바꾸기
+	public int updateStarRate(double star_rate, int movie_id, int user_id);
 
 }
--- src/main/java/com/whychapedia/service/StarRateService.java
+++ src/main/java/com/whychapedia/service/StarRateService.java
@@ -8,12 +8,18 @@ public interface StarRateService {
 
 	List<Integer> selectStarTop(int limit);
 
-	int insertStarRate(int user_id, int movie_id, double star_rate);
+	int insertStarRate(int id,int user_id, int movie_id, double star_rate);
 
 	int deleteStarRate(int user_id, int movie_id);
 
 	int selectMyStarRate(int user_id, int movie_id);
 
 	int selectIsRating(int user_id, int movie_id);
+
+	int selectLastId();
+
+	int updateStarRate(double star_rate, int movie_id, int user_id);
+
+
 
 }
